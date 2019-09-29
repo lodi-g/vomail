f8c18f517081ceac47edba42b407c08d6099b31c
Grégoire Lodi <gregoire.lodi@gmail.com>
2019-09-29 20:08:18 +0200

    Web: removed redundant subject & from

--- web/src/components/MailBox/MailCard.jsx
+++ web/src/components/MailBox/MailCard.jsx
@@ -32,16 +32,16 @@ const MailCard = ({
         <div className="w-100" style={{ cursor: 'pointer' }} onClick={() => onClick(id)}>
           {!read ? (
             <Card.Title>
-              <span>Subject: {subject}</span>
+              <span>{subject}</span>
             </Card.Title>
           ) : (
             <Card.Text>
-              <span>Subject: {subject}</span>
+              <span>{subject}</span>
             </Card.Text>
           )}
           <Card.Text className="d-flex justify-content-between">
             <span>
-              From: {from.name} &lt;{from.address}&gt;
+              {from.name} &lt;{from.address}&gt;
             </span>
             <span>{moment(receivedOn).format('DD MMM YYYY HH:mm')}</span>
           </Card.Text>

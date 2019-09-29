2182c3a382658090c136363cb2f2f5fd42e76220
Grégoire Lodi <gregoire.lodi@gmail.com>
2019-09-29 13:44:42 +0200

    API: cors...............;;

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 const Koa = require('koa')
 const koaBody = require('koa-body')
 const koaStatic = require('koa-static')
+const cors = require('@koa/cors')
 
 const mails = require('./src/mails/')
 const users = require('./src/users/')
 
 const app = new Koa()
 app.use(koaBody())
+app.use(cors())
 
 app.use(mails.routes())
 app.use(users.routes())
